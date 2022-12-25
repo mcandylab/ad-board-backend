f04dc6914e416d195ab2b90b7c9812d6f45ba258
Andrey Abramov <karaulshickov@yandex.ru>
2022-12-25 18:07:50 +0300

    Добавил policy авторизацию для модели Ad

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -5,29 +5,29 @@ namespace App\Http\Controllers;
 use App\Models\Ad;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
-use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
 
 class AdController extends Controller
 {
     public function index()
     {
-        $ads = Ad::with(['user', 'media'])->latest()->paginate(3);
+        $this->authorize('viewAny', Ad::class);
+
+        $ads = Ad::where('user_id', Auth::id())->with(['user', 'media'])->latest()->paginate(3);
 
         return response()->json($ads);
     }
 
     public function show(Ad $ad)
     {
+        $this->authorize('view', $ad);
+
         return response()->json($ad->load(['user', 'media']));
     }
 
-    /**
-     * @throws FileIsTooBig
-     * @throws FileDoesNotExist
-     */
     public function store(Request $request)
     {
+        $this->authorize('create', Ad::class);
+
         $validated = $request->validate([
             'name' => 'required|min:3|max:255',
             'images' => 'required|array|min:1|max:10',
--- app/Policies/AdPolicy.php
+++ app/Policies/AdPolicy.php
@@ -10,85 +10,38 @@ class AdPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
+    public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Ad  $ad
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Ad $ad)
+    public function view(User $user, Ad $ad): bool
     {
-        //
+        return $ad->user_id === $user->id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
+    public function create(User $user): bool
     {
-        //
+        return true;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Ad  $ad
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Ad $ad)
+    public function update(User $user, Ad $ad): bool
     {
-        //
+        return false;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Ad  $ad
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Ad $ad)
+    public function delete(User $user, Ad $ad): bool
     {
-        //
+        return false;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Ad  $ad
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Ad $ad)
+    public function restore(User $user, Ad $ad): bool
     {
-        //
+        return false;
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Ad  $ad
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Ad $ad)
+    public function forceDelete(User $user, Ad $ad): bool
     {
-        //
+        return false;
     }
 }
